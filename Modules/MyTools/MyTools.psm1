a0dcc1e327a6238fbbab0b43e68584d623f066eb
Vladislav Borovikov <vborovikov@live.ru>
2023-04-20 13:19:19 +0500

    added scripts

---
+++ Modules/MyTools/MyTools.psm1
@@ -0,0 +1,49 @@
+# Utility functions
+
+function Find-File
+{
+    [OutputType([IO.FileInfo[]])]
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$FileName
+    )
+    Get-ChildItem -Recurse -Filter "${FileName}" -File -ErrorAction SilentlyContinue
+}
+
+function Open-File
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ParameterSetName='Pipeline')]
+        [IO.FileInfo]$InputObject,
+        [Parameter(Position=0, ParameterSetName='Pipeline')]
+        [Int32[]]$Index = @(),
+        [Parameter(Position=0, ParameterSetName='FileName')]
+        [string]$FileName
+    )
+
+    begin
+    {
+        $editor = 'C:\Program Files\EditPlus\editplus.exe'
+        $skipProcess = $PSBoundParameters.ContainsKey('FileName')
+        if ($skipProcess)
+        {
+            Start-Process -FilePath $editor -ArgumentList @("`"$FileName`"")
+            return
+        }
+        $idx = 0
+    }
+
+    process
+    {
+        if ($skipProcess) {
+            return
+        }
+
+        if ($Index.Contains($idx++) -or $Index.Length -eq 0)
+        {
+            Start-Process -FilePath $editor -ArgumentList @("`"$InputObject`"")
+        }
+    }
+}
---
+++ profile.ps1
@@ -0,0 +1,72 @@
+# Modules
+Import-Module z
+Import-Module Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView -ShowToolTips
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Aliases
+Set-Alias touch New-Item
+Set-Alias file Find-File
+Set-Alias find Find-File
+Set-Alias open Open-File
+Set-Alias edit Open-File
+
+# Prompt
+# Replaces $Home part of the current path with '~'
+# time | path | git | .net
+
+function Prompt {
+    # error
+    $err = $?
+
+    # time
+    $time = (Get-Date).ToString('dd.MM H:mm')
+
+    # path
+    $dir = (Convert-Path .)
+    $path = $dir
+    if ($path.Contains($Home)) {
+        $path = $path.Replace($Home, '~')
+    }
+
+    # git branch
+    $git = (git branch --show-current 2>&1)
+    if ($git -is [Management.Automation.ErrorRecord]) {
+        $git = $null
+    }
+
+    # dotnet
+    $csprojPath = $dir
+    $csproj = $null
+    do {
+        $csproj = Get-ChildItem -Path $csprojPath -Filter '*.csproj' -File -ErrorAction SilentlyContinue
+        if ($csproj -is [IO.FileInfo[]]) {
+            $csproj = $csproj[0]
+            break
+        }
+
+        $csprojPath = Split-Path $csprojPath -Parent
+        if ($csprojPath -eq $Home -or $csprojPath -eq '') {
+            break
+        }
+    } while ($null -eq $csproj)
+    if ($null -ne $csproj) {
+        $csproj = (Select-Xml -Path $csproj -XPath '/Project/PropertyGroup/TargetFramework').Node.InnerText
+        if ($null -ne $csproj) {
+            $csproj = '.' + $csproj
+        }
+    }
+
+    # prompt
+    $promptLevel = if ($NestedPromptLevel -ge 1) { '>>' } else { '>' }
+
+    $(if (Test-Path variable:/PSDebugContext) { '[DBG]: ' } else { '' }) +
+    'PS ' +
+    "`e[36m$time`e[0m" +
+    '|' + "`e[37m$path`e[0m" +
+    $(if ($null -ne $git) { '|' + "`e[92m$git`e[0m" } else { '' }) +
+    $(if ($null -ne $csproj) { '|' + "`e[95m$csproj`e[0m" } else { '' }) +
+    "`r`nPS $(if ($err -ne $true) {"`e[91m$promptLevel`e[0m"} else {$promptLevel}) "
+}
