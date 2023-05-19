544f6aea8f30f84482c6d0ec7a05777d4a210d11
Vladislav Borovikov <vborovikov@live.ru>
2023-05-19 11:30:34 +0500

    added code to display current folder in window title

--- Modules/MyTools/MyTools.psm1
+++ Modules/MyTools/MyTools.psm1
@@ -1,7 +1,6 @@
 # Utility functions
 
-function Find-File
-{
+function Find-File {
     [OutputType([IO.FileInfo[]])]
     [CmdletBinding()]
     param(
@@ -11,38 +10,33 @@ function Find-File
     Get-ChildItem -Recurse -Filter "${FileName}" -File -ErrorAction SilentlyContinue
 }
 
-function Open-File
-{
+function Open-File {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory, ValueFromPipeline, ParameterSetName='Pipeline')]
+        [Parameter(Mandatory, ValueFromPipeline, ParameterSetName = 'Pipeline')]
         [IO.FileInfo]$InputObject,
-        [Parameter(Position=0, ParameterSetName='Pipeline')]
+        [Parameter(Position = 0, ParameterSetName = 'Pipeline')]
         [Int32[]]$Index = @(),
-        [Parameter(Position=0, ParameterSetName='FileName')]
+        [Parameter(Position = 0, ParameterSetName = 'FileName')]
         [string]$FileName
     )
 
-    begin
-    {
+    begin {
         $editor = 'C:\Program Files\EditPlus\editplus.exe'
         $skipProcess = $PSBoundParameters.ContainsKey('FileName')
-        if ($skipProcess)
-        {
+        if ($skipProcess) {
             Start-Process -FilePath $editor -ArgumentList @("`"$FileName`"")
             return
         }
         $idx = 0
     }
 
-    process
-    {
+    process {
         if ($skipProcess) {
             return
         }
 
-        if ($Index.Contains($idx++) -or $Index.Length -eq 0)
-        {
+        if ($Index.Contains($idx++) -or $Index.Length -eq 0) {
             Start-Process -FilePath $editor -ArgumentList @("`"$InputObject`"")
         }
     }
--- profile.ps1
+++ profile.ps1
@@ -31,6 +31,26 @@ function Prompt {
         $path = $path.Replace($Home, '~')
     }
 
+    # title
+    $titlePath = $dir
+    $title = Split-Path $titlePath -Leaf
+    while ($title -eq 'net8.0' -or $title -eq 'net7.0' -or $title -eq 'net6.0' -or
+        $title -eq 'Debug' -or $title -eq 'Release' -or $title -eq 'bin' -or $title -eq 'obj') {
+        # skip this folder name    
+        $titlePath = Split-Path $titlePath -Parent
+        if ($titlePath.Length -eq 0) {
+            break
+        }
+        $title = Split-Path $titlePath -Leaf
+    }
+    if ($title.Length -gt 15) {
+        $title = $title.Substring(0, 15) + "`u{2026}"
+    }
+    if ($title.Length -gt 0) {
+        # set window title
+        $host.UI.RawUI.WindowTitle = $title + ' - PS'
+    }
+
     # git branch
     $git = (git branch --show-current 2>&1)
     if ($git -is [Management.Automation.ErrorRecord]) {
@@ -61,8 +81,8 @@ function Prompt {
         else {
             # old projects
             $csproj = (Select-Xml -Path $csprojPath `
-             -XPath '/vs:Project/vs:PropertyGroup[1]/vs:TargetFrameworkVersion' `
-             -Namespace @{vs='http://schemas.microsoft.com/developer/msbuild/2003'}).Node.InnerText
+                    -XPath '/vs:Project/vs:PropertyGroup[1]/vs:TargetFrameworkVersion' `
+                    -Namespace @{vs = 'http://schemas.microsoft.com/developer/msbuild/2003' }).Node.InnerText
             if ($null -ne $csproj) {
                 $csproj = $csproj.Replace('v', '.net')
             }
@@ -74,9 +94,14 @@ function Prompt {
 
     $(if (Test-Path variable:/PSDebugContext) { '[DBG]: ' } else { '' }) +
     'PS ' +
+    # date and time
     "`e[36m$time`e[0m" +
+    # current path
     '|' + "`e[37m$path`e[0m" +
+    # git status
     $(if ($null -ne $git) { '|' + "`e[92m$git`e[0m" } else { '' }) +
+    # dotnet version
     $(if ($null -ne $csproj) { '|' + "`e[95m$csproj`e[0m" } else { '' }) +
+    # prompt level and error state
     "`r`nPS $(if ($err -ne $true) {"`e[91m$promptLevel`e[0m"} else {$promptLevel}) "
 }
