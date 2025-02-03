7bd8de9ebe77a1ae5551dbd7e5cc15423b2b7395
Vladislav Borovikov <vborovikov@live.ru>
2025-02-03 07:07:18 +0500

    updated git symbols

--- profile.ps1
+++ profile.ps1
@@ -168,14 +168,14 @@ class GitStatus {
                 "$e[2m:$e[22m$(if ($this.Untracked -gt 0) {$this.Untracked} else {"$e[2m0$e[22m"})" +
                 "$e[2m-$e[22m$(if ($this.Deleted -gt 0) {$this.Deleted} else {"$e[2m0$e[22m"})" +
                 "$e[2m]$e[22m$e[0m" +
-                "$e[94m$e[2m@$e[22m$($this.Commit)$e[0m" +
-                "$e[33m$e[2m/$e[22m$($this.Number)$e[0m"
+                "$e[94m$e[2m#$e[22m$($this.Commit)$e[0m" +
+                "$e[33m$e[2m@$e[22m$($this.Number)$e[0m"
         }
 
         return `
             "$e[92m$($this.Branch)$e[0m" +
-            "$e[94m$e[2m@$e[22m$($this.Commit)$e[0m" +
-            "$e[33m$e[2m/$e[22m$($this.Number)$e[0m"
+            "$e[94m$e[2m#$e[22m$($this.Commit)$e[0m" +
+            "$e[33m$e[2m@$e[22m$($this.Number)$e[0m"
     }
 }
 
