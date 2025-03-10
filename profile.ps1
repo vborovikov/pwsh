f2cfc69e964c2b008f35a0dd0fbc2bddcabc3611
Vladislav Borovikov <vborovikov@live.ru>
2025-03-10 12:29:18 +0500

    added error action option

--- profile.ps1
+++ profile.ps1
@@ -24,11 +24,11 @@ Set-Alias edit Open-File
 # Replaces $Home part of the current path with '~'
 # time | path | git | .net
 
-New-Variable PSWindowTitle $Host.UI.RawUI.WindowTitle -Option Constant
+New-Variable PSWindowTitle $Host.UI.RawUI.WindowTitle -Option Constant -ErrorAction SilentlyContinue
 New-Variable SkipTitleNames @(
     'net10.0', 'net9.0', 'net8.0', 'net7.0', 'net6.0', 
     'Debug', 'Release', 'bin', 'obj', 'src', 'test', 'build', 
-    'code', 'game') -Option Constant
+    'code', 'game') -Option Constant -ErrorAction SilentlyContinue
 
 function Prompt {
     $err = $?
