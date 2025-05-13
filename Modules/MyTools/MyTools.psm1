a2f6e1a21b53987aab45ce802302b4835bb2a5c5
Vladislav Borovikov <vborovikov@live.ru>
2025-05-13 18:35:54 +0500

    added Move-LocalDb cmdlet

--- Modules/MyTools/MyTools.psm1
+++ Modules/MyTools/MyTools.psm1
@@ -41,3 +41,49 @@ function Open-File {
         }
     }
 }
+
+# Moves a SQL LocalDB database to a new location
+function Move-LocalDB {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$Database,
+        [Parameter(Mandatory)]
+        [string]$Destination,
+        [Parameter()]
+        [string]$Instance = 'MSSQLLocalDB',
+        [Parameter()]
+        [string]$Path = $HOME
+    )
+
+    if ($null -eq (Get-Command -Name 'sqlcmd' -CommandType Application -ErrorAction SilentlyContinue)) {
+        Write-Error "SqlCmd program not found."
+        return
+    }
+
+    $OldMdfFilePath = Join-Path -Path $Path -ChildPath "$Database.mdf"
+    $OldLdfFilePath = Join-Path -Path $Path -ChildPath "${Database}_log.ldf"
+    if (!(Test-Path -Path $OldMdfFilePath) -or !(Test-Path -Path $OldLdfFilePath)) {
+        Write-Error "File '$OldMdfFilePath' or '$OldLdfFilePath' not found."
+        return
+    }
+
+    $MdfFilePath = Join-Path -Path $Destination -ChildPath "$Database.mdf"
+    $LdfFilePath = Join-Path -Path $Destination -ChildPath "${Database}_log.ldf"
+    if ((Test-Path -Path $MdfFilePath) -or (Test-Path -Path $LdfFilePath)) {
+        Write-Error "File '$MdfFilePath' or '$LdfFilePath' already exists."
+        return
+    }
+
+    # alter database record
+    sqlcmd -S "(LocalDB)\$Instance" -d master -Q "ALTER DATABASE $Database MODIFY FILE (NAME = '$Database', FILENAME = '$MdfFilePath');" -I
+    sqlcmd -S "(LocalDB)\$Instance" -d master -Q "ALTER DATABASE $Database MODIFY FILE (NAME = '${Database}_log', FILENAME = '$LdfFilePath');" -I
+    sqlcmd -S "(LocalDB)\$Instance" -d master -Q "ALTER DATABASE $Database SET OFFLINE WITH ROLLBACK IMMEDIATE;" -I
+
+    # move files
+    Move-Item -Path $OldMdfFilePath -Destination $MdfFilePath -Force
+    Move-Item -Path $OldLdfFilePath -Destination $LdfFilePath -Force
+
+    # bring database online
+    sqlcmd -S "(LocalDB)\$Instance" -d master -Q "ALTER DATABASE $Database SET ONLINE;" -I
+}    
